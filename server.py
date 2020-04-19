0d626d0630006d8f09ac49a355c12699a25361bc
Owen <ocrewe04@gmail.com>
2020-04-19 13:27:50 -0700

    Point system: first attempt

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import os
 import random
+import enum
 
 import cherrypy
 
-"""
-This is a simple Battlesnake server written in Python.
-For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
-"""
-
+class direction(enum.Enum): 
+    left = 0
+    right = 1
+    up = 2
+    down = 3
 
 class Battlesnake(object):
     @cherrypy.expose
@@ -30,12 +31,81 @@ class Battlesnake(object):
         print("START")
         return {"color": "#A06B36", "headType": "safe", "tailType": "freckled"}
 
-    def remove(self, input_list, remove):
-        try:
-            input_list.remove(remove)
-        except:
-            pass
-        return input_list
+    def collision_check(self, points, data, pt_change):
+        head = data['you']['body'][0]
+        snakes = data["board"]["snakes"]
+
+        #Check for walls
+        if(head['x'] == 0):
+            points[direction.left.value] += pt_change
+        if(head['y'] == 0):
+            points[direction.up.value] += pt_change
+        if(head['x'] == data["board"]["width"] - 1):
+            points[direction.right.value] += pt_change
+        if(head['y'] == data["board"]["height"] - 1):
+            points[direction.down.value] += pt_change
+
+        #Don't hit snakes, including youself
+        for snake in snakes:
+            for i in range(len(snake["body"])-1):
+                segment = snake["body"][i]
+                if(segment["y"] == head["y"]):
+                    if(segment["x"] == head["x"] - 1):
+                        points[direction.left.value] += pt_change
+                    elif(segment["x"] == head["x"] + 1):
+                        points[direction.right.value] += pt_change
+                elif(segment["x"] == head["x"]):
+                    if(segment["y"] == head["y"] - 1):
+                        points[direction.up.value] += pt_change
+                    elif(segment["y"] == head["y"] + 1):
+                        points[direction.down.value] += pt_change
+
+    def nearby_heads(self, points, data, pt_change_prey, pt_change_pred):
+        head = data['you']['body'][0]
+        snakes = data["board"]["snakes"]
+
+        #avoid predators, seek prey
+        for snake in snakes:
+            if(snake["id"] == data['you']['id']):
+                continue
+            elif(len(snake["body"]) < len(data['you']['body'])):
+                pt_change = pt_change_prey
+            else:
+                pt_change = pt_change_pred
+
+            enemy_head = snake["body"][0]
+            if(enemy_head["x"] == head["x"] - 2 and enemy_head["y"] == head["y"]):   #left 2
+                points[direction.left.value] += pt_change
+            elif(enemy_head["x"] == head["x"] + 2 and enemy_head["y"] == head["y"]):   #right 2
+                points[direction.right.value] += pt_change
+            elif(enemy_head["x"] == head["x"] and enemy_head["y"] == head["y"] - 2):   #up 2
+                points[direction.up.value] += pt_change
+            elif(enemy_head["x"] == head["x"] and enemy_head["y"] == head["y"] + 2):   #down 2
+                points[direction.down.value] += pt_change
+            elif(enemy_head["x"] == head["x"] - 1 and enemy_head["y"] == head["y"] - 1):   #left 1 up 1
+                points[direction.left.value] += pt_change
+                points[direction.right.value] += pt_change
+            elif(enemy_head["x"] == head["x"] - 1 and enemy_head["y"] == head["y"] + 1):   #left 1 down 1
+                points[direction.left.value] += pt_change
+                points[direction.down.value] += pt_change
+            elif(enemy_head["x"] == head["x"] + 1 and enemy_head["y"] == head["y"] - 1):   #right 1 up 1
+                points[direction.right.value] += pt_change
+                points[direction.right.value] += pt_change
+            elif(enemy_head["x"] == head["x"] + 1 and enemy_head["y"] == head["y"] + 1):   #right 1 down 1
+                points[direction.right.value] += pt_change
+                points[direction.down.value] += pt_change
+
+    def outer_tiles(self, points, data, pt_change):
+        head = data['you']['body'][0]
+        #Preference for the middle
+        if(head['x'] == 1):
+            points[direction.left.value] += pt_change
+        if(head['y'] == 1):
+            points[direction.up.value] += pt_change
+        if(head['x'] == data["board"]["width"] - 2):
+            points[direction.right.value] += pt_change
+        if(head['y'] == data["board"]["height"] - 2):
+            points[direction.down.value] += pt_change
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -43,94 +113,30 @@ class Battlesnake(object):
     def move(self):
         # This function is called on every turn of a game. It's how your snake decides where to move.
         # Valid moves are "up", "down", "left", or "right".
-        # TODO: Use the information in cherrypy.request.json to decide your next move.
         data = cherrypy.request.json
 
-        # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
-        favourable_moves = ["up", "down", "left", "right"]
-        
-        head = data['you']['body'][0]
-        body = data['you']['body']
+        #left right up down
+        points = [0]*4
 
-        print(f"Head pos: {head}")
+        #check for definite collisions
+        self.collision_check(points, data, -1000)
 
-        #Check for walls
-        if(head['x'] == 0):
-            possible_moves.remove("left")
-        if(head['y'] == 0):
-            possible_moves.remove("up")
-        if(head['x'] == data["board"]["width"] - 1):
-            possible_moves.remove("right")
-        if(head['y'] == data["board"]["height"] - 1):
-            possible_moves.remove("down")
+        self.nearby_heads(points, data, 2, -6)
 
-        #Don't hit snakes
-        snakes = data["board"]["snakes"]
-        for snake in snakes:
-            for i in range(len(snake["body"])-1):
-                segment = snake["body"][i]
-                if(segment["y"] == head["y"]):
-                    if(segment["x"] == head["x"] - 1):
-                        possible_moves = self.remove(possible_moves, "left")
-                    elif(segment["x"] == head["x"] + 1):
-                        possible_moves = self.remove(possible_moves, "right")
-                elif(segment["x"] == head["x"]):
-                    if(segment["y"] == head["y"] - 1):
-                        possible_moves = self.remove(possible_moves, "up")
-                    elif(segment["y"] == head["y"] + 1):
-                        possible_moves = self.remove(possible_moves, "down")
-
-        #Big snake evasion
-        for snake in snakes:
-            if(len(snake["body"]) < len(body) or snake["id"] == data['you']['id']):
-                continue
-            enemy_head = snake["body"][0]
-            if(enemy_head["x"] == head["x"] - 2 and enemy_head["y"] == head["y"]):   #left 2
-                favourable_moves = self.remove(favourable_moves, "left")
-            elif(enemy_head["x"] == head["x"] + 2 and enemy_head["y"] == head["y"]):   #right 2
-                favourable_moves = self.remove(favourable_moves, "right")
-            elif(enemy_head["x"] == head["x"] and enemy_head["y"] == head["y"] - 2):   #up 2
-                favourable_moves = self.remove(favourable_moves, "up")
-            elif(enemy_head["x"] == head["x"] and enemy_head["y"] == head["y"] + 2):   #down 2
-                favourable_moves = self.remove(favourable_moves, "down")
-            elif(enemy_head["x"] == head["x"] - 1 and enemy_head["y"] == head["y"] - 1):   #left 1 up 1
-                favourable_moves = self.remove(favourable_moves, "left")
-                favourable_moves = self.remove(favourable_moves, "up")
-            elif(enemy_head["x"] == head["x"] - 1 and enemy_head["y"] == head["y"] + 1):   #left 1 down 1
-                favourable_moves = self.remove(favourable_moves, "left")
-                favourable_moves = self.remove(favourable_moves, "down")
-            elif(enemy_head["x"] == head["x"] + 1 and enemy_head["y"] == head["y"] - 1):   #right 1 up 1
-                favourable_moves = self.remove(favourable_moves, "right")
-                favourable_moves = self.remove(favourable_moves, "up")
-            elif(enemy_head["x"] == head["x"] + 1 and enemy_head["y"] == head["y"] + 1):   #right 1 down 1
-                favourable_moves = self.remove(favourable_moves, "right")
-                favourable_moves = self.remove(favourable_moves, "down")
-
-        #Preference for the middle
-        if(head['x'] == 1):
-            favourable_moves = self.remove(favourable_moves, "left")
-        if(head['y'] == 1):
-            favourable_moves = self.remove(favourable_moves, "up")
-        if(head['x'] == data["board"]["width"] - 2):
-            favourable_moves = self.remove(favourable_moves, "right")
-        if(head['y'] == data["board"]["height"] - 2):
-            favourable_moves = self.remove(favourable_moves, "down")
-
-        #handle favourable, yet non-possible moves
-        directions = favourable_moves.copy()
-        for direction in directions:
-            if(direction not in possible_moves):
-                favourable_moves.remove(direction)
+        self.outer_tiles(points, data, -1)
 
         #choose
-        if(len(possible_moves)==0):
+        move_choices = []
+        max_pts = max(points)
+        for i in range(4):
+            if(points[i] == max_pts):
+                move_choices.append(direction(i).name)
+        #choose
+        if(len(move_choices)==0):
             print("OH SHIT! Guess I'll just die then.")
             move = "up"
-        elif(len(favourable_moves)==0):
-            move = random.choice(possible_moves)
         else:
-            move = random.choice(favourable_moves)
+            move = random.choice(move_choices)
 
         print(f"MOVE: {move}")
         return {"move": move}
