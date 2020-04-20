775decc399837360ebbfc3cf976589fc6bd433b7
Owen <ocrewe04@gmail.com>
2020-04-19 21:13:42 -0700

    debugging

--- server.py
+++ server.py
@@ -148,8 +148,8 @@ class Battlesnake(object):
         #passive food grabbing
         self.adjacent_food(points, data, 2)
 
-        sneks = data["board"]["snakes"]
-        print(f"snakes: {sneks}")   
+        sneks = len(data["board"]["snakes"])
+        print(f"# of snakes: {sneks}")   
         #get moves with most points
         move_choices = []
         max_pts = max(points)
