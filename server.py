9f2a45d194619ad9d6ffa2a2bb68ab241bad11e6
Owen <ocrewe04@gmail.com>
2020-04-25 13:27:03 -0700

    colour change

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ class Battlesnake(object):
         # cherrypy.request.json contains information about the game that's about to be played.
         data = cherrypy.request.json
         print("START")
-        return {"color": "#A06B36", "headType": "safe", "tailType": "freckled"}
+        return {"color": "#F1F1F1", "headType": "safe", "tailType": "freckled"}
 
     def collision_check(self, points, data, pt_change):
         head = data['you']['body'][0]
