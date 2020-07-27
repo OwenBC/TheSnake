eed72a2cdfc276744f95b4400f68ac7e7baef0c9
Owen <ocrewe04@gmail.com>
2020-07-27 13:00:00 -0700

    changed port to 80

--- server.py
+++ server.py
@@ -188,7 +188,7 @@ if __name__ == "__main__":
     server = Battlesnake()
     cherrypy.config.update({"server.socket_host": "0.0.0.0"})
     cherrypy.config.update(
-        {"server.socket_port": int(os.environ.get("PORT", "8080")),}
+        {"server.socket_port": 80}
     )
     print("Starting Battlesnake Server...")
     cherrypy.quickstart(server)
