e0a4d87ede9d2cebc958bd3fda517e54c98bfdc7
Owen <ocrewe04@gmail.com>
2020-09-06 18:50:50 -0700

    initial implementation - a* algorithm - a* based food chasing and tail chasing - backup desperation move - TODO: check for larger openents nearby

--- board.py
+++ board.py
@@ -1,32 +1,35 @@
+import astar
+import random
+
 '''
-x - empty
-M - my head
-B - my body
-H - larger or equivalent enemy head
-h - smaller enemy head
-b - enemy body
-x - tails
-F - food
+0 - empty
+0 - tails
+3 - my head
+4 - my body
+5 - larger or equivalent enemy head
+2 - smaller enemy head
+4 - enemy body
+1 - food
+-1 - OOB
 '''
 
 class Board:
-
 	def __init__(self, data):
-		self.board = [['x' for y in range(data['board']['height'])] for x in range(data['board']['width'])]
 		self.width = data['board']['width']
 		self.height = data['board']['height']
+		self.board = [[0 for y in range(self.height)] for x in range(self.width)]
+		for food in data['board']['food']:
+			self.__setPos(food['x'],food['y'], 1)
 		for snake in data['board']['snakes']:
 			if(snake['id']==data['you']['id']):
-				self.__addSnakeToBoard(snake, 'M','B')
+				self.__addSnakeToBoard(snake, 3)
 			elif(snake['length'] < data['you']['length']):
-				self.__addSnakeToBoard(snake, 'h','b')
+				self.__addSnakeToBoard(snake, 2)
 			else:
-				self.__addSnakeToBoard(snake, 'H','b')
-		for food in data['board']['food']:
-			self.__setPos(food['x'],food['y'],'F')
+				self.__addSnakeToBoard(snake, 5)
 
 
-	def __addSnakeToBoard(self, snake, head, body):
+	def __addSnakeToBoard(self, snake, head):
 		i = 1
 		length = snake['length']
 		for segment in snake['body']:
@@ -35,15 +38,81 @@ class Board:
 			if(i == 1):
 				self.__setPos(x,y,head)
 			elif(i < length):
-				self.__setPos(x,y,body)
+				self.__setPos(x,y,4)
 			i+=1
-
 
 	def __setPos(self, x, y, val):
 		self.board[x][y] = val
 
-	def getPos(self, x, y):
-		if(x<0 or y<0 or x>self.width-1 or y>self.height-1):
-			return 'W'
+	#returns value stored at the given location on the board
+	def getPos(self, pos):
+		if(pos[0]<0 or pos[1]<0 or pos[0]>self.width-1 or pos[1]>self.height-1):
+			return -1
 		else:
-			return self.board[x][y]
+			return self.board[pos[0]][pos[1]]
+
+	def __get_move(self, start, end):
+		if start[0] == end[0]:
+			if start[1] > end[1]:
+				return "down"
+			else:
+				return "up"
+		else:
+			if start[0] > end[0]:
+				return "left"
+			else:
+				return "right"
+
+	def __return_first_move(self, path):
+		self.__get_move(path[0], path[1])
+
+	def __astarPath(self, start, end):
+		return astar.astar(self.board, start, end)
+
+	#check path for two closest foods and return first move of shortest path
+	def seek_food(self, pos, food):
+		targets = []
+		while len(targets) < min(2, len(food)):
+			temp_targets = []  
+			min_dist = self.width + self.height
+			for f in food:
+				dist = abs(pos[0]-food["x"]) + abs(pos[1]-food["y"])
+				if(dist < min_dist):
+					min_dist = dist
+					temp_targets = [f]
+				elif(dist == min_dist):
+					temp_targets.append(f)
+			for f in temp_targets:
+				food.remove(f)
+				targets.append(f)
+		astarPaths = []
+		for f in targets:
+			f_pos = (f["x"],f["y"])
+			path = self.__astarPath(pos, f_pos)
+			if path == None:
+				continue
+			astarPaths.append([len(path), self.__return_first_move(path)])
+		if len(astarPaths) == 0:
+			return None
+		target = astarPaths[0]
+		for f in astarPaths:
+			if f[0] < target[0]:
+				target = f
+		return target[1]
+
+	#tell me how to get to back end
+	def coil(self, head, tail):
+		return self.__return_first_move(self.__astarPath(head, tail))
+
+	#pick an empty adjacent move
+	def desperation(self,head):
+		x = head[0]
+		y = head[1]
+		options = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+		for d in options:
+			val = self.getPos(d)
+			if val > 2 or val == -1:
+				options.remove(d)
+		if len(options) == 0:
+			return None
+		return self.__get_move(head, random.choice(options))
--- server.py
+++ server.py
@@ -1,132 +1,32 @@
 import os
 import random
-import enum
 
 import cherrypy
-from board import *
-
-class direction(enum.Enum): 
-    left = 0
-    right = 1
-    up = 2
-    down = 3
+from board import Board
 
 class Battlesnake(object):
     @cherrypy.expose
+    @cherrypy.tools.json_out()
     def index(self):
         # If you open your snake URL in a browser you should see this message.
-        return "If you're reading this it's working"
-
-    @cherrypy.expose
-    def ping(self):
-        # The Battlesnake engine calls this function to make sure your snake is working.
-        return "pong"
+        return {
+            "apiversion": "1",
+            "author": "owenc",
+            "color": "#F1F1F1",  
+            "head": "dead",  
+            "tail": "block-bum",  
+        }
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
     def start(self):
         # This function is called everytime your snake is entered into a game.
         # cherrypy.request.json contains information about the game that's about to be played.
+        # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
+
         print("START")
-        return {"color": "#F1F1F1", "headType": "dead", "tailType": "block-bum"}
-
-    def collision_check(self, points, x, y, width, height, board, pt_change):
-        #Check for walls
-        if(x == 0):
-            points[direction.left.value] += pt_change
-        if(y == 0):
-            points[direction.up.value] += pt_change
-        if(x == width - 1):
-            points[direction.right.value] += pt_change
-        if(y == height - 1):
-            points[direction.down.value] += pt_change
-
-        #Don't hit snakes, including youself
-        if(board.getPos(x-1,y) != 'x' and board.getPos(x-1,y) != 'F'):
-            points[direction.left.value] += pt_change
-        if(board.getPos(x+1,y) != 'x' and board.getPos(x+1,y) != 'F'):
-            points[direction.right.value] += pt_change
-        if(board.getPos(x,y-1) != 'x' and board.getPos(x,y-1) != 'F'):
-            points[direction.up.value] += pt_change
-        if(board.getPos(x,y+1) != 'x' and board.getPos(x,y+1) != 'F'):
-            points[direction.down.value] += pt_change
-
-    def adj_heads(self, points, x, y, board, pt_change_prey, pt_change_pred):
-        #avoid predators, seek prey
-        pts = [['H',pt_change_pred],['h',pt_change_prey]]
-        for target in pts:
-            if(board.getPos(x-2,y) == target[0]):   #left 2
-                points[direction.left.value] += target[1]
-            if(board.getPos(x+2,y) == target[0]):   #right 2
-                points[direction.right.value] += target[1]
-            if(board.getPos(x,y-2) == target[0]):   #up 2
-                points[direction.up.value] += target[1]
-            if(board.getPos(x,y+2) == target[0]):   #down 2
-                points[direction.down.value] += target[1]
-            if(board.getPos(x-1,y-1) == target[0]):   #left 1 up 1
-                points[direction.left.value] += target[1]
-                points[direction.up.value] += target[1]
-            if(board.getPos(x-1,y+1) == target[0]):   #left 1 down 1
-                points[direction.left.value] += target[1]
-                points[direction.down.value] += target[1]
-            if(board.getPos(x+1,y-1) == target[0]):   #right 1 up 1
-                points[direction.right.value] += target[1]
-                points[direction.up.value] += target[1]
-            if(board.getPos(x+1,y+1) == target[0]):   #right 1 down 1
-                points[direction.right.value] += target[1]
-                points[direction.down.value] += target[1]
-
-    def outer_tiles(self, points, x, y, width, height, pt_change):
-        #Preference for the middle
-        if(x==1):
-            points[direction.left.value] += pt_change
-        elif(x == width - 2):
-            points[direction.right.value] += pt_change
-        elif(x == 0 or x == width - 1):
-            points[direction.up.value] += pt_change/2
-            points[direction.down.value] += pt_change/2
-        if(y == 1):
-            points[direction.up.value] += pt_change
-        elif(y == height - 2):
-            points[direction.down.value] += pt_change
-        elif(y == 0 or y == height - 1):
-            points[direction.left.value] += pt_change/2
-            points[direction.right.value] += pt_change/2
-
-    def adjacent_food(self, points, x, y, board, pt_change):
-        #point bonus for nearby food
-        if(board.getPos(x-1,y) == 'F'):
-            points[direction.left.value] += pt_change
-        if(board.getPos(x+1,y) == 'F'):
-            points[direction.right.value] += pt_change
-        if(board.getPos(x,y-1) == 'F'):
-            points[direction.up.value] += pt_change
-        if(board.getPos(x,y+1) == 'F'):
-            points[direction.down.value] += pt_change
-
-    def seek_food(self, points, food, x, y, width, height, pt_change):
-        #favour closest food
-        min_dist = width + height
-        indices = []
-        for i in range(len(food)):
-            dist = abs(x-food[i]["x"]) + abs(y-food[i]["y"])
-            if(dist < min_dist):
-                min_dist = dist
-                indices = [i]
-            elif(dist == min_dist):
-                indices.append(i)
-        if(min_dist < (width + height)/2):
-            target_food = food[random.choice(indices)]
-            if(target_food["x"]<x):
-                points[direction.left.value] += pt_change
-            elif(target_food["x"]>x):
-                points[direction.right.value] += pt_change
-            if(target_food["y"]<y):
-                points[direction.up.value] += pt_change
-            elif(target_food["y"]>y):
-                points[direction.down.value] += pt_change
+        return "ok"
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -139,37 +39,23 @@ class Battlesnake(object):
         #create Board object
         gameboard = Board(data)
 
-        #left right up down
-        points = [0]*4
-
         #get stuff from data
-        x,y = data['you']['body'][0]['x'],data['you']['body'][0]['y']
-        width,height = data["board"]["width"],data["board"]["height"]
-
-        #check for definite collisions
-        self.collision_check(points, x, y, width, height, gameboard, -1000)
-        #chase smaller snakes, avoid bigger ones
-        self.adj_heads(points, x, y, gameboard, 3, -9)
-        #preference to stay away from walls
-        self.outer_tiles(points, x, y, width, height, -1)
-        #food grabbing
-        if(data['you']['health']<95): #active
-            self.seek_food(points, data["board"]["food"], x, y, width, height, 2)
-        else: #passive
-            self.adjacent_food(points, x, y, gameboard, 2)
-
-        #get moves with most points
+        head = (data['you']['body'][0]['x'], data['you']['body'][0]['y'])
+        tail = (data['you']['body'][-1]['x'], data['you']['body'][-1]['y'])
         
-        move_choices = []
-        max_pts = max(points)
-        for i in range(4):
-            if(points[i] == max_pts):
-                move_choices.append(direction(i).name)
-        print(f"points: {points}")   
-        print(f"move choices: {move_choices}")
+        move = None
 
-        #choose
-        move = random.choice(move_choices)
+        #hungr, look for food
+        if(data["you"]["health"] < 20):
+            move = gameboard.seek_food(head, data["board"]["food"])
+
+        #chase tail
+        if(move == None):
+            move = gameboard.coil(head, tail)
+        if(move == None):
+            move = gameboard.desperation(head)
+        if(move == None):
+            move = "down"
 
         print(f"MOVE: {move}")
         return {"move": move}
