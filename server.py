2bc9f08502c33339080e99b000ddcd3355138d1b
Owen <ocrewe04@gmail.com>
2020-10-01 00:17:45 -0700

    Changed server port for heroku testing

--- server.py
+++ server.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     server = Battlesnake()
     cherrypy.config.update({"server.socket_host": "0.0.0.0"})
     cherrypy.config.update(
-        {"server.socket_port": 80}
+        {"server.socket_port": 8080}
     )
     print("Starting Battlesnake Server...")
     cherrypy.quickstart(server)
